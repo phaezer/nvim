683fd9873a9941430b5fcca8cc7986b1a7fc63fb
Eric Austin <eric.m.austin@gmail.com>
2025-09-10 21:12:18 -0500

    cspell moved to lsp, no longer need null-ls/none-ls for cspell

--- lua/phaezer/plugins/lsp/none-ls.lua
+++ lua/phaezer/plugins/lsp/none-ls.lua
@@ -1,39 +1,40 @@
--- NOTE: nonel-ls replaces null-ls
+-- NOTE: none-ls replaces null-ls
 return {
   'nvimtools/none-ls.nvim',
+  enabled = false, -- not needed when using native cspell lsp
   lazy = true,
   event = 'VeryLazy',
   dependencies = {
     'nvim-lua/plenary.nvim',
     -- NOTE: provides cspell integration
-    'davidmh/cspell.nvim',
+    -- 'davidmh/cspell.nvim',
   },
   opts = function(_, opts)
-    local cspell = require 'cspell'
-    opts.sources = opts.sources or {}
-    table.insert(
-      opts.sources,
-      cspell.diagnostics.with {
-        diagnostics_postprocess = function(diagnostic)
-          diagnostic.severity = vim.diagnostic.severity.HINT
-        end,
-      }
-    )
-    table.insert(opts.sources, cspell.code_actions)
+    -- local cspell = require 'cspell'
+    -- opts.sources = opts.sources or {}
+    -- table.insert(
+    --   opts.sources,
+    --   cspell.diagnostics.with {
+    --     diagnostics_postprocess = function(diagnostic)
+    --       diagnostic.severity = vim.diagnostic.severity.HINT
+    --     end,
+    --   }
+    -- )
+    -- table.insert(opts.sources, cspell.code_actions)
+    --
+    -- local ns = require 'null-ls'
+    -- local map = require('phaezer.core.keys').map
 
-    local ns = require 'null-ls'
-    local map = require('phaezer.core.keys').map
-
-    -- toggle on and off sources
-    map {
-      prefix = '<leader>l',
-      plugin = 'none-ls',
-      {
-        's',
-        function() ns.toggle { name = 'cspell' } end,
-        desc = 'toggle cspell',
-      },
-    }
+    --   -- toggle on and off sources
+    --   map {
+    --     prefix = '<leader>l',
+    --     plugin = 'none-ls',
+    --     {
+    --       's',
+    --       function() ns.toggle { name = 'cspell' } end,
+    --       desc = 'toggle cspell',
+    --     },
+    --   }
   end,
-  init = function() end,
+  -- init = function() end,
 }
